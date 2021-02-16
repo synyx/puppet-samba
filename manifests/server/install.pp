32048c92446a0a4cc555a7be350ae82aa1ee363d
Max Ferstl <ferstl@synyx.de>
2021-02-16 12:19:20 +0100

    Provide stable version for debian via apt.van-belle.nl

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,6 +1,21 @@
 # == Class samba::server::install
 #
 class samba::server::install {
+  if fact('os.family') == 'Debian' {
+    apt::source { 'samba':
+      location => 'https://apt.van-belle.nl/debian buster-samba413',
+      release  => 'main contrib non-free',
+      repos    => '',
+      key      => {
+        id     => '3843C126C596738E7C8775B5E6F6A3C7EB7A89CF',
+        source => 'https://apt.van-belle.nl/louis-van-belle.gpg-key.asc',
+      },
+      include  => {
+        src => false,
+        deb => true,
+      },
+    }
+  }
   package { 'samba':
     ensure => installed
   }
