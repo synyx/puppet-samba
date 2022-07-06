bb215aa1873d9d415dfe7004811783555b127d2b
Fabian Werner <werner@synyx.de>
2022-07-06 15:34:37 +0200

    use stable version via apt.van-belle.nl only for buster and older

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -2,18 +2,24 @@
 #
 class samba::server::install {
   if fact('os.family') == 'Debian' {
-    apt::source { 'samba':
-      location => "https://apt.van-belle.nl/debian ${facts['lsbdistcodename']}-samba413",
-      release  => 'main contrib non-free',
-      repos    => '',
-      key      => {
-        id     => '3843C126C596738E7C8775B5E6F6A3C7EB7A89CF',
-        source => 'https://apt.van-belle.nl/louis-van-belle.gpg-key.asc',
-      },
-      include  => {
-        src => false,
-        deb => true,
-      },
+    if versioncmp($::operatingsystemrelease, '11') >= 0 {
+      apt::source { 'samba':
+        ensure => absent,
+      }
+    } else {
+      apt::source { 'samba':
+        location => "https://apt.van-belle.nl/debian ${facts['lsbdistcodename']}-samba413",
+        release  => 'main contrib non-free',
+        repos    => '',
+        key      => {
+          id     => '3843C126C596738E7C8775B5E6F6A3C7EB7A89CF',
+          source => 'https://apt.van-belle.nl/louis-van-belle.gpg-key.asc',
+        },
+        include  => {
+          src => false,
+          deb => true,
+        },
+      }
     }
   }
   package { 'samba':
