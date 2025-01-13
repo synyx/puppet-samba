c8c88feea07337ed836fbec2238dd8283343b59f
Fabian Werner <werner@synyx.de>
2025-01-13 11:10:09 +0100

    [samba::server::params] resolve 'Unknown variable' Error for strict mode

--- manifests/server/params.pp
+++ manifests/server/params.pp
@@ -2,7 +2,10 @@
 #
 class samba::server::params {
   case $facts['os']['family'] {
-    'Redhat': { $service_name = 'smb' }
+    'Redhat': {
+      $service_name = 'smb'
+      $nmbd_name = undef
+    }
     'Debian': {
       case $facts['os']['name'] {
         'Debian': {
@@ -11,15 +14,22 @@ class samba::server::params {
           } else {
             $service_name = 'samba'
           }
+          $nmbd_name = undef
         }
         'Ubuntu': {
           $service_name = 'smbd'
           $nmbd_name = 'nmbd'
         }
-        default: { $service_name = 'samba' }
+        default: {
+          $service_name = 'samba'
+          $nmbd_name = undef
+        }
       }
     }
-    'Gentoo': { $service_name = 'samba' }
+    'Gentoo': {
+      $service_name = 'samba'
+      $nmbd_name = undef
+    }
     'Archlinux': {
       $service_name = 'smbd'
       $nmbd_name = 'nmbd'
